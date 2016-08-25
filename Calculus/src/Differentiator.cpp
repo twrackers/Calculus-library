c0869f1f26133c05dddc44ae1ac17b0a62cceff0
Thomas W Rackers <twrackers@users.noreply.github.com>
2016-08-25 15:56:43 -0400

    Changed calls to Cache object
    Cache class's step method now returns samples as double*, so separate calls to step and getSamples are not necessary.

--- Calculus/src/Differentiator.cpp
+++ Calculus/src/Differentiator.cpp
@@ -8,12 +8,10 @@ Differentiator::Differentiator() : m_cache(Cache(3))
 
 double Differentiator::step(double y)
 {
-  m_cache.update(y);
-  double* yv = m_cache.getSamples();
+  double* yv = m_cache.step(y);
   double& y0 = *yv;
   double& ym1 = *(yv + 1);
   double& ym2 = *(yv + 2);
   m_quad.solve(ym2, ym1, y0);
   return m_quad.deriv() / TimeBase::getTimeStep();
 }
-
