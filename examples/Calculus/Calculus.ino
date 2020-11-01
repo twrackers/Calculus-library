7d55e66a2dcb120e2299769eb2e381b817b6c3e5
Thomas W Rackers <twrackers@users.noreply.github.com>
2020-11-01 01:30:57 -0400

    Update example sketch

--- examples/Calculus/Calculus.ino
+++ examples/Calculus/Calculus.ino
@@ -1,4 +1,4 @@
-#include <math.h>
+//#include <math.h>
 
 #include <Pulser.h>
 #include <StateMachine.h>
@@ -14,25 +14,42 @@ const double dt = (double) sampleTime / 1000.0;   // sec
 const double rateLimit = 5.0;                     // (y-units)/sec
 const double tau = 1.0 / rateLimit;               // sec
 
+// 'pulser' will return true for 1 sec, false for 2 sec.
 Pulser pulser(1000, 2000);
-TimeStep ts(dt);
-LowPass lpf(ts, tau);
-RateLimiter limiter(ts, rateLimit);
+// 'updater' sets pace of output.
 StateMachine updater(sampleTime, true);
+
+// Define time step for LowPass and RateLimiter objects.
+TimeStep ts(dt);
+// Low-pass filter object
+LowPass lpf(ts, tau);
+// Rate-limiter object
+RateLimiter limiter(ts, rateLimit);
 
 void setup() 
 {
+  // Start Serial output, setting baud rate.
+  // This must match setting in Serial Monitor
+  // or Serial Plotter (both under Tools menu).
   Serial.begin(115200);
 }
 
 void loop()
 {
+  // Update the pulser object which generates the
+  // input signal.
+  pulser.update();
+  // Time to iterate and output?
   if (updater.update()) {
-    pulser.update();
+    // Input to following steps, toggles between 0 and 1.
     double r = pulser.read() ? 1.0 : 0.0;
+    // Do a low-pass filter on the input.
     double y = lpf.step(r);
+    // Do a rate-limiter on the input.
     double z = limiter.step(r);
-    // Use 'Tools => Serial Plotter' to see the output.
+    // Print the results.
+    // Use 'Tools => Serial Plotter' or
+    // 'Tools => Serial Plotter' to see the output.
     Serial.print(r);
     Serial.print(" ");
     Serial.print(y);
